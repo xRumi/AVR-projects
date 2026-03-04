5b3715b15c9fcfe8984df10dd2f8be1f5827d04f
Rumi <mhxrumi@gmail.com>
2026-03-04 17:38:53 +0600

    attiny45 led blink with timer0 and interrupt

---
+++ ATTiny45 LED blink Timer0 INT ASM/attiny45.asm
@@ -0,0 +1,40 @@
+.equ RAMEND, 0x15F
+
+.equ PINB, 0x16
+.equ PINB0, 0
+.equ PINB1, 1
+.equ PINB2, 2
+.equ PINB3, 3
+.equ PINB4, 4
+.equ PINB5, 5
+
+.equ DDRB, 0x17
+.equ DDB0, 0
+.equ DDB1, 1
+.equ DDB2, 2
+.equ DDB3, 3
+.equ DDB4, 4
+.equ DDB5, 5
+
+.equ PORTB, 0x18
+.equ PB0, 0
+.equ PB1, 1
+.equ PB2, 2
+.equ PB3, 3
+.equ PB4, 4
+.equ PB5, 5
+
+.equ SPH, 0x3E
+.equ SPL, 0x3D
+
+.equ TCNT0, 0x32
+.equ TCCR0A, 0x2A
+.equ TCCR0B, 0x33
+.equ OCR0A, 0x29
+
+.equ TIMSK, 0x39
+.equ OCIE0A, 4
+
+.equ TIFR, 0x38
+.equ TOV0, 1
+.equ OCF0A, 4
